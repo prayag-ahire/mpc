2b412563ca761fdcbeb63ee34641fff264388de1
prayag <prayagahire365@gmail.com>
2025-04-28 22:27:26 +0530

    solve bugs in products and add search , hooks , types

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { Button } from "@/components/button";
+import { Skeleton } from "@/components/skeleton";
+import type { Product } from "@/types/producttype";
+import axios from "axios";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Product(){
+
+const params = useParams<{id:string}>();
+const router = useRouter();
+const { id } = params;
+const [value,setValue] = useState<Product | null>(null);
+const [loading,setLoading] = useState(true);
+
+
+const fetchData = async()=>{
+    try{
+        const res = await axios.get(`https://fakestoreapi.com/products/${id}`);
+        setValue(res.data);
+    }catch(error){
+        
+      }finally{
+        setLoading(false);
+      } 
+}
+useEffect(()=>{
+    if(id)
+    fetchData();
+},[id])
+
+const handler = ()=>{
+    
+    router.push("/");
+}
+if(loading){
+    return(<div className="flex flex-col h-screen text-4xl ">
+      <Skeleton/>
+    </div>)
+  }
+return(<div>
+    <div><Button onclick={handler}>Back</Button></div>
+        <div className="grid grid-cols-3 max-w-full py-5" >
+            <div className="col-span-1" ><Image alt={`${value?.title}`} width={300} height={100} src={`${value?.image}`}></Image></div>
+            <div className="col-span-2 ">
+                <div className="text-2xl font-bold pb-10">{value?.title}</div>
+                <div className="w-full pb-10">{value?.description}</div>
+                <div className=""><p>{value?.rating.rate} ratings</p></div>
+                <div><p className="text-2xl font-bold ">${value?.price}</p></div>
+                <div><p>Stock remaning {value?.rating.count}</p></div>
+            </div>
+        </div>
+    </div>)
+}
---
+++ hooks/useStore.ts
@@ -0,0 +1,26 @@
+import { Product } from "@/types/producttype";
+import { create } from "zustand";
+
+type State = {
+    value: Product,
+    updateValues:(value:Product) => void
+}
+
+export const useProductStore = create<State>((set)=>({
+    value: {
+        id:0,
+        title:"",
+        description:"",
+        category:"",
+        image:"",
+        price:0,
+        rating:{
+            count:0,
+            rate:0
+        }
+    },
+
+    updateValues: (value:Product)=>set(()=>({
+        value:value
+    }))
+}))
